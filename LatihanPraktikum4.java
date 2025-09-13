7ec679d56fb17b65f9e901b5fe09b2ce36dcd51b
ilyasmanaja <ilyasmansaja@gmail.com>
2025-09-13 14:50:13 +0700

    Add files via upload

---
+++ LatihanPraktikum4.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.praktikum4;
+import java.util.Scanner;
+/**
+ *
+ * @author Raihan
+ */
+public class LatihanPraktikum4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int pilihan;
+        char kembali;
+
+        do {
+            System.out.println("\nCalculator Bangun Datar");
+            System.out.println("1. Luas Persegi");
+            System.out.println("2. Luas Lingkaran");
+            System.out.println("3. Luas Segitiga");
+            System.out.println("4. Luas Persegi Panjang");
+            System.out.println("5. Keluar");
+            System.out.print("Masukkan pilihan Anda: ");
+            pilihan = scanner.nextInt();
+
+            switch (pilihan) {
+                case 1:
+                    System.out.println("Menghitung luas Persegi");
+                    System.out.print("Masukkan sisi: ");
+                    int sisi = scanner.nextInt();
+                    int luasPersegi = sisi * sisi;
+                    System.out.println("Luas persegi adalah " + luasPersegi + " cm");
+                    break;
+                case 2:
+                    final double PI = 3.14;
+                    System.out.println("Menghitung luas lingkaran");
+                    System.out.print("Masukkan jari jari: ");
+                    int jari = scanner.nextInt();
+                    double luasLingkaran = PI * jari * jari;
+                    System.out.println("Luas lingkaran adalah " + luasLingkaran + " cm");
+                    break;
+                case 3:
+                    final double setengah = 0.5;
+                    System.out.println("Menghitung luas Segitiga");
+                    System.out.print("Masukkan alas: ");
+                    int alas = scanner.nextInt();
+                    System.out.print("Masukkan tinggi: ");
+                    int tinggi = scanner.nextInt();
+                    double luasSegitiga = setengah * alas * tinggi;
+                    System.out.println("Luas segitiga adalah " + luasSegitiga + " cm");
+                    break;
+                case 4:
+                    System.out.println("Menghitung luas Persegi Panjang");
+                    System.out.print("Masukkan Panjang: ");
+                    int panjang = scanner.nextInt();
+                    System.out.print("Masukkan Lebar: ");
+                    int lebar = scanner.nextInt();
+                    int luasPersegiPanjang = panjang * lebar;
+                    System.out.println("Luas persegi panjang adalah " + luasPersegiPanjang + " cm");
+                    break;
+                case 5:
+                    System.out.println("Terima kasih telah menggunakan kalkulator ini. Sampai jumpa!");
+                    scanner.close();
+                    return; // Mengakhiri program
+                default:
+                    System.out.println("Pilihan tidak valid. Silakan coba lagi.");
+                    break;
+            }
+
+            // Meminta konfirmasi untuk kembali ke menu
+            System.out.print("Apakah ingin melakukan perhitungan lagi? (y/n): ");
+            kembali = scanner.next().charAt(0);
+        } while (kembali == 'y' || kembali == 'Y');
+
+    }
+    
+}
---
+++ Praktikum4.java
@@ -0,0 +1,146 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praktikum4;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Raihan
+ */
+public class Praktikum4 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // if - else if - else
+        System.out.print("Masukan nilai: ");
+        int nilai = in.nextInt();
+
+        if (nilai > 75){
+            System.out.println("Anda lulus ujian");
+        } else if (nilai > 65) {
+            System.out.println("Nilai anda berada di ambang batas kelulusan");
+        } else {
+            System.out.println("Anda tidak lulus ujian");
+        }
+
+        System.out.println();
+
+        // Switch case
+        System.out.print("Masukan warna: ");
+        String warna = in.next();
+
+        switch (warna) {
+            case "R":
+                System.out.println("Warna merah");
+                break;
+
+            case "G":
+                System.out.println("Warna hijau");
+                break;
+
+            case "B":
+                System.out.println("Warna biru");
+                break;
+
+            default:
+                System.out.println("Tidak ada warna yang dipilih");
+                break;
+        }
+
+        System.out.println();
+
+        // Nested if
+        System.out.print("Masukan angka :");
+        int angka = in.nextInt();
+
+        if (angka > 0) {
+            System.out.print("Angka " + angka + " bernilai positif");
+            if (angka % 2 == 0) {
+                System.out.println(" dan genap");
+            } else {
+                System.out.println(" dan ganjil");
+            }
+        } else if (angka == 0) {
+            System.out.println("Merupakan angka 0");
+        } else {
+            System.out.print("Angka " + angka + " bernilai negatif");
+            if (angka % 2 == 0) {
+                System.out.println(" dan genap");
+            } else {
+                System.out.println(" dan ganjil");
+            }
+        }   
+    
+
+        System.out.println();
+
+        //For loop
+        for (int i = 1;  i <= 3 ; i++) {
+            System.out.println("For loop ke-" + i);
+        }
+
+        String[] nama= {"nama", "namaqd"};
+        for (int j = 0; j < nama.length; j++) {
+            System.out.println(nama[j]);
+        }
+
+        System.out.println();
+
+        // While Loop
+        int b = 1;
+        while (b <= 3) {
+            System.out.println("While loop ke-" + b);
+            b++;
+        }
+
+        System.out.println();
+
+        // Do While
+        int c = 6;
+        do {
+            System.out.println("Do while loop ke-" + c);
+            c++;
+        } while (c <= 5);
+
+        System.out.println();
+
+        // Nested loop
+        char kursi = 'A';
+
+        for (int j = 0; j < 3; j++) {
+            for (int j2 = 0; j2 < 5; j2++) {
+                System.out.print(kursi + "" + j2 + " ");
+            }
+        }
+
+        // for (int j3 = 0; int j4; j3 < 5; j4 < 5 ; j3++ ; j4++ ) {
+        //         System.out.print(kursi + "" + j + " ");
+        //     }
+
+        System.out.println();
+        System.out.println();
+
+        // Break
+        for (int k = 1; k <= 10; k++) {
+            if (k == 5) {
+                System.out.println("Break di angka ke-" + k);
+                break ;
+            }
+            System.out.println("Angka ke-" + k);
+        }
+
+        System.out.println();
+
+        // Continue
+        for (int h = 1; h <= 10; h++) {
+            if (h % 2 == 0) {
+                continue;
+            }
+            System.out.println("Bilangan ganjil " + h);
+        }
+
+    }
+}
