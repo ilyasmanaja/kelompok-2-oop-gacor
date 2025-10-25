cb4ec58d302df9370ef20dac8a62be442f362464
radityayhoripratama <radityayhori208@gmail.com>
2025-10-25 20:41:51 +0700

    Add files via upload

---
+++ Latihan Praktikum 8/CalculatorMain.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class CalculatorMain{
+    public static   void main(String args[]){
+        SwingUtilities.invokeLater(() -> {
+            new PixelCalculator().setVisible(true);
+        });
+    }
+}
